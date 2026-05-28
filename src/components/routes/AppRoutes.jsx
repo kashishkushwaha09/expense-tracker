de06d6a1844febe15f90b19c0d26d8af28f1e8f2
Khushboo Kachhi <kashishkushwaha03.com>
2026-05-29 00:30:00 +0530

    add ProtectedRoute and PublicRoute components for route protection

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -2,12 +2,19 @@ import React, { lazy, Suspense } from "react";
 import Layout from "../layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import { Spinner } from "react-bootstrap";
-import Login from "../../pages/Login";
-import Home from "../../pages/Home";
-import CompleteProfile from "../../pages/CompleteProfile";
-import ForgotPassword from "../../pages/ForgotPassword";
 
-const AuthPage=lazy(()=>import("../../pages/AuthPage"))
+import ProtectedRoute from "./ProtectedRoute";
+import PublicRoute from "./PublicRoute";
+
+const AuthPage = lazy(() => import("../../pages/AuthPage"));
+const Login = lazy(() => import("../../pages/Login"));
+const Home = lazy(() => import("../../pages/Home"));
+const CompleteProfile = lazy(() =>
+  import("../../pages/CompleteProfile")
+);
+const ForgotPassword = lazy(() =>
+  import("../../pages/ForgotPassword")
+);
 const AppRoutes = () => {
   return (
     <Layout>
@@ -15,26 +22,37 @@ const AppRoutes = () => {
         fallback={
           <div style={{ textAlign: "center", marginTop: "50px" }}>
             <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
           </div>
         }
       >
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-        </Routes>
-        
-          <Routes>
-            <Route path="/complete-profile" element={<CompleteProfile/>}/>
-        </Routes>
         <Routes>
-            <Route path="/signup" element={<AuthPage/>}/>
-        </Routes>
-        <Routes>
-            <Route path="/login" element={<Login/>}/>
-        </Routes>
-        <Routes>
-            <Route path="/forgot-password" element={<ForgotPassword/>}/>
+          {/* Protected Routes */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/complete-profile"
+            element={
+              <ProtectedRoute>
+                <CompleteProfile />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Public Routes */}
+          <Route path="/signup" element={<PublicRoute><AuthPage /></PublicRoute>} />
+
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+
+          <Route path="/forgot-password" element={<PublicRoute><ForgotPassword /></PublicRoute>} />
         </Routes>
       </Suspense>
     </Layout>

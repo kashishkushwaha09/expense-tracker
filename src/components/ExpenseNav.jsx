78156072ac0b0da45e6651879a65ae3b5ca388cd
Khushboo Kachhi <kashishkushwaha03.com>
2026-05-28 23:55:34 +0530

    add authentication handling in navigation and implement logout functionality

--- src/components/ExpenseNav.jsx
+++ src/components/ExpenseNav.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import React, { useContext } from 'react'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 const ExpenseNav = () => {
+    const navigate = useNavigate();
+     const {token,logout}=useContext(AuthContext);
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
   return (
     <>
       <Navbar className="bg-body-tertiary">
         <Container fluid>
           <Navbar.Brand href="#home">Expense Tracker</Navbar.Brand>
             <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-             <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/signup">Sign up</Nav.Link>
+           {token && <Nav.Link href="/">Home</Nav.Link>}
+            {!token && <Nav.Link href="/login">Login</Nav.Link>}
+              {!token &&  <Nav.Link href="/signup">Sign up</Nav.Link>}
+             { token && <Button
+              variant="danger"
+              size="sm"
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>}
             {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
           </Nav>
         </Container>
